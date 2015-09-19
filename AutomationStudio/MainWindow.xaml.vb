82411960d80f7694e513f3a42d1025cf52dd6e18
piyushnet <piyushnet@live.com>
2015-09-19 17:17:48 -0400

    First Upload

---
+++ AutomationStudio/MainWindow.xaml.vb
@@ -0,0 +1,12 @@
+﻿Class MainWindow
+    Inherits Elysium.Controls.Window
+
+
+
+
+    Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
+       
+    End Sub
+
+
+End Class
---
+++ AutomationStudio/Welcome.xaml.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Threading
+
+Public Class Welcome
+
+    Private Async Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
+
+        Await Task.Delay(1500)
+
+        Dim IDE As MainWindow = New MainWindow
+        IDE.Show()
+        Me.Close()
+
+    End Sub
+
+    Private IsAliveTimer As Timers.Timer
+
+
+
+End Class
